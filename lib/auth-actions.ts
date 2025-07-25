44ed2926572433692f52c2db2335092b0bc60efc
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-07-25 22:41:37 +0500

    Removing Errors

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -2,7 +2,7 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
+  await signIn("google", { redirectTo: "/" });
 };
 
 export const logout = async () => {
